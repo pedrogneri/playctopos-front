c0cf37548088310641acac6d243796cd5617e6cc
pedrogneri <pedrogneri@gmail.com>
2020-07-27 19:08:53 -0300

    Setup socket.io

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import socket from '../../socket';
 
 import {
   Container,
+  VideoPlayer,
+  CenteredContainer,
+  Input,
 } from './styles';
 
+socket.on('connect', () => {
+  console.log('[IO] Connect => New connection');
+});
+
 const Home = () => {
+  const [videoId, setVideoId] = useState('c0kfcP_nD9E');
+
+  const handleSearch = (event) => {
+    setVideoId(event.target.value);
+  };
+
   return (
-    <Container>Bom dia</Container>
+    <Container>
+      <CenteredContainer>
+        <label>Digite o ID do video</label>
+        <Input type="text" value={videoId} onChange={handleSearch} />
+        <VideoPlayer
+          allow="autoplay"
+          title="Youtube video player"
+          src={`https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&start=186`}>
+        </VideoPlayer>
+      </CenteredContainer>
+    </Container>
   );
 };
 
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -2,5 +2,39 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: row;
+  height: 100vh;
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
+`;
+
+export const CenteredContainer = styled.div`
+  display: flex;
   flex-direction: column;
+  justify-content: center;
+  width: 50%;
+  height: 100vh;
+  padding: 16px;
+
+  @media (max-width: 800px) {
+    height: 50%;
+    width: 100%;
+    padding: 0;
+    justify-content: flex-start;
+  }
+`;
+
+export const VideoPlayer = styled.iframe`
+  width: 100%;
+  height: 50%;
+  pointer-events: none;
+  border: none;
+`;
+
+export const Input = styled.input`
+  padding: 5px;
+  margin-bottom: 16px;
+  max-width: 50%;
 `;
