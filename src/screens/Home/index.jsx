86cacdb41dd582002a25f314d0cce444425abd13
pedrogneri <pedrogneri@gmail.com>
2020-07-29 03:55:54 -0300

    Remove connection console log

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -8,10 +8,6 @@ import {
   VideoContainer,
 } from './styles';
 
-socket.on('connect', () => {
-  console.log('[IO] Connect => New connection');
-});
-
 const Home = () => {
   useEffect(() => {
     socket.emit('room.join', 1);
