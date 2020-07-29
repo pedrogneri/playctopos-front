b035f8a1b6f1fba7e3acb095fe5387d1f4aef112
pedrogneri <pedrogneri@gmail.com>
2020-07-29 03:53:54 -0300

    Remove unnecessary styledComponents

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -6,7 +6,7 @@ export const Container = styled.div`
   height: 100vh;
   background-color: #313131;
 
-  @media (max-width: 600px) {
+  @media (max-width: 800px) {
     flex-direction: column;
   }
 `;
@@ -18,30 +18,6 @@ export const VideoContainer = styled.div`
   height: 100vh;
 
   @media (max-width: 800px) {
-    height: 50%;
     width: 100%;
-    justify-content: flex-start;
   }
-`;
-
-export const VideoMask = styled.div`
-  display: flex;
-  width: 100%;
-  height: 60%;
-  background-color: #414141;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const VideoPlaceholder = styled.p`
-  font-size: 36px;
-  color: #ddd;
-  text-align: center;
-`;
-
-export const VideoPlayer = styled.iframe`
-  width: 100%;
-  height: 60%;
-  pointer-events: none;
-  border: none;
 `;
