d6049a2475ab4d75db41d3f1ca8061eee39e4446
pedrogneri <pedrogneri@gmail.com>
2020-07-29 20:43:41 -0300

    Create transitionModal component

---
+++ src/components/TransitionModal/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Backdrop, Fade } from '@material-ui/core';
+
+import { StyledModal as Modal } from './styles';
+
+const TransitionModal = ({ children, show, onClose }) => (
+  <Modal
+    open={show}
+    onClose={onClose}
+    closeAfterTransition
+    BackdropComponent={Backdrop}
+    BackdropProps={{
+      timeout: 500,
+    }}
+  >
+    <Fade in={show}>
+      {children}
+    </Fade>
+  </Modal>
+);
+
+TransitionModal.propTypes = {
+  children: PropTypes.node,
+  show: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired
+};
+
+export default TransitionModal;
