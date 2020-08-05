c726ab32f05b0406dce226bbe544e06425e79c94
Pedro Neri <pedrogneri@gmail.com>
2020-08-05 19:51:09 -0300

    Add room service

---
+++ src/services/room.js
@@ -0,0 +1,23 @@
+import { get, post } from 'axios';
+
+const baseURL = 'http://localhost:8080';
+
+export const getRoom = async (id) => {
+  const response = await get(`${baseURL}/room`, {
+    params: {
+      id,
+    }
+  });
+  return response.data;
+};
+
+export const updateRoom = async (id, room) => {
+  const response = await post(`${baseURL}/room?id=${id}`, 
+    room,
+    {
+      params: {
+        id,
+      }
+    });
+  return response.data;
+};
