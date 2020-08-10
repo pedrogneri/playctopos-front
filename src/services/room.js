769ba656cd373a32ab2018d5eec8c01a743dbf26
Pedro Neri <pedrogneri@gmail.com>
2020-08-10 02:53:41 -0300

    Just..

--- src/services/room.js
+++ src/services/room.js
@@ -3,7 +3,7 @@ import { get, post } from 'axios';
 const baseURL = process.env.REACT_APP_API;
 
 export const getRoom = async (id) => {
-  const response = await get(`${baseURL}room`, {
+  const response = await get(`${baseURL}/room`, {
     params: {
       id,
     },
@@ -12,7 +12,7 @@ export const getRoom = async (id) => {
 };
 
 export const updateRoom = async (id, room) => {
-  const response = await post(`${baseURL}room?id=${id}`, room, {
+  const response = await post(`${baseURL}/room?id=${id}`, room, {
     params: {
       id,
     },
