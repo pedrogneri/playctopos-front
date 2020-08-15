8935f7c7358e5094e7364bca3ecfd3cb77ea4d85
Pedro Neri <pedrogneri@gmail.com>
2020-08-15 18:11:48 -0300

    Create videoInfo component

---
+++ src/screens/components/Room/components/VideoPlayer/components/VideoInfo/index.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import { Grid, Hidden, IconButton } from '@material-ui/core';
+import PropTypes from 'prop-types';
+
+import { Container, SkipIcon, Title, Subtitle, Thumbnail, InfoContainer, PlaylistIcon } from './styles';
+
+const VideoInfo = ({ title, channel, thumbnail, onShowPlaylist, onSkip }) => {
+  return (
+    <Container container alignItems="center">
+      <Grid container xs={12} md={6} direction="row" alignItems="center">
+        <Grid xs={5} sm={3} xl={2}>
+          <Thumbnail src={thumbnail} />
+        </Grid>
+
+        <InfoContainer xs={7} sm={9} xl={10}>
+          <Hidden smDown>
+            <Title>{title}</Title>
+            <Subtitle>{channel}</Subtitle>
+          </Hidden>
+          <Hidden mdUp>
+            <Title.xs>{title}</Title.xs>
+            <Subtitle.xs>{channel}</Subtitle.xs>
+          </Hidden>
+        </InfoContainer>
+      </Grid>
+
+      <Grid container xs={12} md={6} justify="flex-end">
+        <IconButton onClick={onShowPlaylist}>
+          <PlaylistIcon />
+        </IconButton>
+
+        <IconButton onClick={onSkip}>
+          <SkipIcon />
+        </IconButton>
+      </Grid>
+    </Container>
+  );
+};
+
+VideoInfo.propTypes = {
+  title: PropTypes.string.isRequired,
+  channel: PropTypes.string.isRequired,
+  thumbnail: PropTypes.string.isRequired,
+  onShowPlaylist: PropTypes.func.isRequired,
+  onSkip: PropTypes.func.isRequired,
+};
+
+export default VideoInfo;
---
+++ src/screens/components/Room/components/VideoPlayer/components/VideoInfo/styles.js
@@ -0,0 +1,52 @@
+import { IoMdSkipForward } from 'react-icons/io';
+import { MdFeaturedPlayList } from 'react-icons/md';
+
+import { Grid } from '@material-ui/core';
+import styled, { css } from 'styled-components';
+
+export const Container = styled(Grid)`
+  padding: 12px;
+  height: 100%;
+`;
+
+export const InfoContainer = styled(Grid)`
+  padding-left: 12px;
+`;
+
+export const Title = styled.h3`
+  color: #fff;
+  margin-bottom: 12px;
+`;
+
+export const Subtitle = styled.h5`
+  color: #fff;
+  font-weight: normal;
+`;
+
+Title.xs = styled.h5`
+  color: #fff;
+  margin-bottom: 6px;
+`;
+
+Subtitle.xs = styled.p`
+  color: #fff;
+`;
+
+export const IconStyle = css`
+  height: 30px;
+  width: 30px;
+  color: #fff !important;
+`;
+
+export const SkipIcon = styled(IoMdSkipForward)`
+  ${IconStyle}
+`;
+
+export const PlaylistIcon = styled(MdFeaturedPlayList)`
+  ${IconStyle}
+`;
+
+export const Thumbnail = styled.img`
+  width: 100%;
+  max-width: 200px;
+`;
