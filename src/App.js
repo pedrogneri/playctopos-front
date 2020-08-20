54604e3cdac38ed28d7bec1e1b41bb1baeae554b
Pedro Neri <pedrogneri@gmail.com>
2020-08-19 22:59:28 -0300

    Create home route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Home from 'screens/components/Home';
 import Room from 'screens/containers/Room';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route exact path="/" component={Home} />
         <Route path="/room/:id" component={Room} />
-        <Route path="/">
-          <Redirect to="/room/5f28c42f6239e613afc82b12" />
-        </Route>
       </Switch>
     </Router>
   );
