5d858423223d1d5880273e9a9a9d367946bf6599
Pedro Neri <pedrogneri@gmail.com>
2020-08-21 22:39:34 -0300

    Create routes file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+
+import Routes from 'routes';
 
 import { ToastProvider } from 'components/Toast/toastContext';
-import Home from 'screens/containers/Home';
-import Room from 'screens/containers/Room';
 
 function App() {
   return (
     <ToastProvider>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/room/:id" component={Room} />
-          <Route path="*">
-            <Redirect to="/" />
-          </Route>
-        </Switch>
-      </Router>
+      <Routes />
     </ToastProvider>
   );
 }
