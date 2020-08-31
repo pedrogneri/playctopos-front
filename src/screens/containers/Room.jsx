a8646adb51a0ff099ba0d66915b7685c513022ca
Pedro Neri <pedrogneri@gmail.com>
2020-08-30 22:26:17 -0300

    Add validate Room Id to room

--- src/screens/containers/Room.jsx
+++ src/screens/containers/Room.jsx
@@ -1,20 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import PropTypes from 'prop-types';
 
 import Room from 'screens/components/Room';
 import { sendWarn } from 'services/chat';
-import { joinRoom } from 'services/room';
+import { joinRoom, getVideoUrlByRoom } from 'services/room';
 import { getUsername } from 'utils/username';
 
 const RoomContainer = ({ match: { params } }) => {
   const { id } = params;
+  const history = useHistory();
+
+  const validateRoomId = useCallback(() => {
+    getVideoUrlByRoom(id)
+      .then((room) => {
+        if (!room) {
+          history.push('/');
+        }
+      })
+      .catch(() => {
+        history.push('/');
+      });
+  }, [id, history]);
 
   useEffect(() => {
     const username = getUsername() || 'Someone';
+    validateRoomId();
     joinRoom(id);
     sendWarn(id, `${username} joined the party`);
-  }, [id]);
+  }, [id, validateRoomId]);
 
   return <Room id={id} />;
 };
