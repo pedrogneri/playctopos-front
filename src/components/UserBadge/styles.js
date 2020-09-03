e7de3e7392355b33722c1df2e302338f093a7f1b
Pedro Neri <pedrogneri@gmail.com>
2020-09-02 21:41:18 -0300

    Create UserBadge component

---
+++ src/components/UserBadge/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 8px 0;
+`;
+
+export const UserLabel = styled.div`
+  border-radius: 12px;
+  padding: 6px;
+  font-size: 10px;
+
+  border: 1px solid #ca3e47;
+  color: #ca3e47;
+
+  max-width: max-content;
+`;
