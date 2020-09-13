2ad474c57e2999df67e147cf5e5f33aec28a721a
Pedro Neri <pedrogneri@gmail.com>
2020-09-13 17:26:05 -0300

    Add secondary colors to theme file

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,13 @@
 export const Theme = {
   white: '#fff',
   primary: {
-    default: '#ca3e47',
-    light: '',
-  },
-  secondary: {
     default: '#424242',
     light: '#525252',
     dark: '#313131',
+  },
+  secondary: {
+    default: '#ca3e47',
+    light: '#d5666d',
+    dark: '#a72e36',
   },
 };
