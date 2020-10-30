96bb60f50bc733b70177dfd70b917a065375dd00
Pedro Neri <pedrogneri@gmail.com>
2020-10-30 03:22:36 -0300

    Add Store and WebSockets providers on index

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { StoreProvider } from 'easy-peasy';
 import Routes from 'routes';
+import store from 'store';
 import { ThemeProvider } from 'styled-components';
 import { Theme } from 'theme';
 
 import { ToastProvider } from 'components/Toast/toastContext';
+import WebSocketContextProvider from 'hooks/useSockets';
 
 import * as serviceWorker from './serviceWorker';
 import { GlobalStyle } from './styles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={Theme}>
-      <>
-        <GlobalStyle />
-        <ToastProvider>
-          <Routes />
-        </ToastProvider>
-      </>
-    </ThemeProvider>
+    <StoreProvider store={store}>
+      <WebSocketContextProvider>
+        <ThemeProvider theme={Theme}>
+          <>
+            <GlobalStyle />
+            <ToastProvider>
+              <Routes />
+            </ToastProvider>
+          </>
+        </ThemeProvider>
+      </WebSocketContextProvider>
+    </StoreProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
