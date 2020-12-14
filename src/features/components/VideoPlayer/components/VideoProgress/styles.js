2c37e90f2c56f8a1172ea81e5043f47fa84329b7
Pedro Neri <pedrogneri@gmail.com>
2020-12-14 18:27:56 -0300

    Remove border-radius from videoProgress

--- src/features/components/VideoPlayer/components/VideoProgress/styles.js
+++ src/features/components/VideoPlayer/components/VideoProgress/styles.js
@@ -10,7 +10,6 @@ export const Container = styled.div`
 
 export const BaseBar = styled.div`
   display: flex;
-  border-radius: 12px;
   background-color: ${({ theme }) => theme.primary.light};
   width: 100%;
   height: 100%;
@@ -20,7 +19,6 @@ export const ProgressBar = styled.div`
   width: ${({ value }) => `${value}%`};
   height: 100%;
   background-color: ${({ theme }) => theme.secondary.default};
-  border-radius: 12px;
   transition: width 1s;
 `;
 
