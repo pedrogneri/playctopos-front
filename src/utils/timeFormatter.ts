ab293d99d85c08f679c1a7d272fc5b3c2888c1e3
Pedro Neri <pedrogneri@gmail.com>
2021-01-31 15:34:08 -0300

    Fix lint utils files

--- src/utils/timeFormatter.ts
+++ src/utils/timeFormatter.ts
@@ -7,16 +7,16 @@ export const getFormattedTimeBySeconds = (seconds: number) => {
 };
 
 const formatDuration = (duration: Duration) => {
-  let items = [('0' + duration.minutes()).slice(-2), ('0' + duration.seconds()).slice(-2)];
+  let items = [(`0${duration.minutes()}`).slice(-2), (`0${duration.seconds()}`).slice(-2)];
 
   if (duration.hours() > 0) {
-    items = [('0' + duration.hours()).slice(-2), ...items];
+    items = [(`0${duration.hours()}`).slice(-2), ...items];
   }
   if (duration.days() > 0) {
-    items = [('0' + duration.days()).slice(-2), ...items];
+    items = [(`0${duration.days()}`).slice(-2), ...items];
   }
   if (duration.months() > 0) {
-    items = [('0' + duration.months()).slice(-2), ...items];
+    items = [(`0${duration.months()}`).slice(-2), ...items];
   }
 
   return items.join(':');
--- src/utils/username.ts
+++ src/utils/username.ts
@@ -1,6 +1,4 @@
-export const getUsername = () => {
-  return localStorage.getItem('@username');
-};
+export const getUsername = () => localStorage.getItem('@username');
 
 export const changeUsername = (newUsername: string) => {
   localStorage.setItem('@username', newUsername);
