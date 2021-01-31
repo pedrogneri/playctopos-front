ec0a7774c3ced668f9a54b607d0c9702c4e0ade8
Pedro Neri <pedrogneri@gmail.com>
2021-01-31 15:29:42 -0300

    Turn hooks files into typescript

---
+++ src/hooks/useSockets/index.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, useContext } from 'react';
+
+import { useStoreActions } from 'easy-peasy';
+import socket from 'socket';
+import { Message } from 'models/message';
+import { Store } from 'store';
+import { sendMessage } from 'services/chat';
+
+type ContextValue = {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  handleSendMessage: Function;
+}
+
+const WebSocketContext = createContext<ContextValue | null>(null);
+
+export const useSockets = () => useContext(WebSocketContext);
+
+type Props = {
+  children: React.ReactNode,
+}
+
+const WebSocketContextProvider = ({ children }: Props) => {
+  const addMessageAction = useStoreActions<Store>(({ addMessage }) => addMessage);
+
+  const handleSendMessage = (roomId: string, message: Message) => {
+    sendMessage(roomId, message);
+  };
+
+  socket.on('room.message', ({ message }: { message: Message }) => {
+    addMessageAction(message);
+  });
+
+  return (
+    <WebSocketContext.Provider value={{ handleSendMessage }}>
+      {children}
+    </WebSocketContext.Provider>
+  );
+};
+
+export default WebSocketContextProvider;
